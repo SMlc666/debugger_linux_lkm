e80fc443c9e642aedafe5d8af16ce60c8927eba2
SMlc666 <qq1030452538@outlook.com>
2026-03-29 00:11:38 +0800

    android: fix memory screen saveable import

--- android/app/src/main/java/com/smlc666/lkmdbg/ui/screens/MemoryScreen.kt
+++ android/app/src/main/java/com/smlc666/lkmdbg/ui/screens/MemoryScreen.kt
@@ -25,8 +25,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
