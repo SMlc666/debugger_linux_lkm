4548ce1087092e99e8dafcd5a2095d3c80b07df3
SMlc666 <qq1030452538@outlook.com>
2026-03-28 15:20:33 +0800

    android: fix launcher theme resources

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(project(":shared"))
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.activity.compose)
